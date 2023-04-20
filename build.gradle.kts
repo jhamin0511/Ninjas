5b3ab8e0869a6d9c1441bfdf8b566b659cf0576b
jhamin0511 <jhamin0511@spendit.kr>
2023-04-20 15:39:33 +0900

    feat: Ktlint 적용
    https://github.com/jlleitschuh/ktlint-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,32 @@ plugins {
     alias(libs.plugins.android.application) apply (false)
     alias(libs.plugins.android.library) apply (false)
     alias(libs.plugins.kotlin) apply (false)
+    alias(libs.plugins.ktlint) apply (false)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+    register("build", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+    register("copyGitHooks", Copy::class) {
+        from("/config/git/hooks/") {
+            include("**/*")
+            rename("(.*)", "$1")
+        }
+        into("/.git/hooks")
+    }
+    named("clean") {
+        dependsOn(":copyGitHooks")
+    }
 }
